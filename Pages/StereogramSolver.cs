30bb4f2041642ac9d0564bab12451d1d0a5a55d8
Lubos Dohnal <e-lubos.dohnal@embedit.com>
2025-04-20 12:29:31 +0200

    StereogramSolverPresetSelectionTest

--- Pages/StereogramSolver.cs
+++ Pages/StereogramSolver.cs
@@ -1,3 +1,4 @@
+using Melown.Core;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 
@@ -9,9 +10,22 @@ public class StereogramSolver(IWebDriver driver)
     public IWebDriver Driver { get; set; } = driver;
     public WebDriverWait Wait { get; set; } = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
     public string Title => Driver.Title;
+    public IWebElement PresetSelect => Driver.FindWebElement(Driver, By.Id("preset-select"));
+    public IWebElement SourceImage => Driver.FindWebElement(Driver, By.CssSelector("img"));
 
     public void NavigateTo()
     {
         Driver.Navigate().GoToUrl(Url);
+    }
+
+    public void SelectPreset(int index)
+    {
+        SelectElement select = new(PresetSelect);
+        select.SelectByIndex(index);
+    }
+
+    public string GetSourceImageSrc()
+    {
+        return SourceImage.GetAttribute("src") ?? string.Empty;
     }
 }
--- Tests.cs
+++ Tests.cs
@@ -7,28 +7,42 @@ namespace Melown;
 public class Tests
 {
     protected IWebDriver? WebDriver { get; set; }
+    protected StereogramSolver stereogramSolver = null!;
 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
         WebDriver = DriverFactory.CreateDriver(false);
+        stereogramSolver = new(WebDriver!);
     }
 
-    [Test]
-    public void StereogramSolverTest()
+    [Test, Order(1)]
+    public void StereogramSolverPageLoadTest()
     {
+        stereogramSolver.NavigateTo();
+        Assert.That(stereogramSolver.Title, Is.EqualTo("Stereogram solver"), "Title does not match.");
+    }
+
+    [Test, Order(2)]
+    public void StereogramSolverPresetSelectionTest()
+    {
+        string[] expectedImages = ["shark", "thumbsup", "planet", "dolphins", "atomium"];
+
         using (Assert.EnterMultipleScope())
         {
-            StereogramSolver homePage = new(WebDriver!);
-            homePage.NavigateTo();
-            Assert.That(homePage.Title, Is.EqualTo("Stereogram solver"), "Title does not match.");
+            for (int i = 0; i < expectedImages.Length; i++)
+            {
+                stereogramSolver.SelectPreset(i + 1);
+                Thread.Sleep(500); //process javaScript
+                Assert.That(stereogramSolver.GetSourceImageSrc(), Does.Contain(expectedImages[i]), "Source image src is incorrect.");
+            }
         }
     }
 
     [OneTimeTearDown]
     public void OneTimeTearDown()
     {
-        Thread.Sleep(5000);
+        Thread.Sleep(2000); //demo
         WebDriver?.Quit();
         WebDriver?.Dispose();
     }
